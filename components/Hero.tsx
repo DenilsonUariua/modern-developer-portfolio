34684f95ef0ccad128a542f62a8c9267d74de0d2
Denilson Uariua <denuariua@gmail.com>
2024-06-05 15:57:39 +0200

    Implements hero section

---
+++ components/Hero.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className='pb-20 pt-36'>
+        <div>
+            Hero Section
+        </div>
+    </div>
+  )
+}
+
+export default Hero
