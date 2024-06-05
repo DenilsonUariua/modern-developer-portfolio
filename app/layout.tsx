b011182e5a431a6a458036d7930d6ea4c523f4d9
Denilson Uariua <denuariua@gmail.com>
2024-06-05 15:32:52 +0200

    impletments the starter template of the application more work needs to be done to complete it in the future anyways back to the banking application

---
+++ app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Denilson's Portfolio ",
+  description:
+    "A modern but definitely not minimalist portfolio for the modern developer.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="relative flex h-screen justify-center items-center bg-black-100">
+      <div className="font-bold text-gray-700 text-2xl">
+        <h1>Hello, Portfolio</h1>
+      </div>
+    </main>
+  );
+}
